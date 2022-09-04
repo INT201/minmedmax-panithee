50b034045128b4564eb39339f78405f9087f69d7
panithee <panithee.pnt@mail.kmutt.ac.th>
2022-09-04 23:36:37 +0700

    test git

--- minMedMax.js
+++ minMedMax.js
@@ -2,5 +2,14 @@ const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
   //code here
+  //code here
+  
 }
+
+// console.log(minMedMax(85, 30, 1));
+// console.log(minMedMax(10, 0, 20));
+// console.log(minMedMax(-5, 0, 10));
+// console.log(minMedMax(5, 1, 1));
+// console.log(minMedMax(-1, -8, 0));
+
 module.exports = minMedMax
