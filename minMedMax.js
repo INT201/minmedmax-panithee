4f04ae508eed2f2b59c756b4744ed667d63b5aa8
panithee <panithee.pnt@mail.kmutt.ac.th>
2022-09-11 22:49:28 +0700

    Fixed

--- minMedMax.js
+++ minMedMax.js
@@ -2,7 +2,6 @@ const { template } = require("@babel/core");
 
 function minMedMax(n1, n2, n3) {
   //code here
-
   if (n1 > n2 && n1 > n3 && n2 > n3) {
     return {min: n3, mid: n2, max: n1};
   } else if (n1 > n2 && n1 > n3 && n3 > n2) {
@@ -15,11 +14,11 @@ function minMedMax(n1, n2, n3) {
     return {min: n2, mid:  n1, max: n3};
   } else if (n3 > n1 && n3 > n2 && n2 > n1) {
     return {min: n1, mid: n2, max: n3};
-  } else if (n1 == n2 && n1 < n3) {
+  } else if (n1 === n2 && n1 < n3) {
     return {min: n1, mid: n3, max: n3};
-  } else if (n2 == n3 && n2 < n1) {
+  } else if (n2 === n3 && n2 < n1) {
     return {min: n2, mid: n1, max: n1};
-  } else if (n1 == n3 && n1 < n2) {
+  } else if (n1 === n3 && n1 < n2) {
     return {min: n1, mid: n2, max: n2};
   } else {
     return {min: n1, mid: n1, max: n1};
